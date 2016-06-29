95f28ccd713a08d2d398e1226efcfd175dce7dc9
Anthony Snelling <Anthony Snelling>
2016-06-29 16:26:35 -0400

    initial commit

---
+++ BuidingEscape/Source/BuidingEscape/BuidingEscape.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuidingEscape.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BuidingEscape, "BuidingEscape" );
